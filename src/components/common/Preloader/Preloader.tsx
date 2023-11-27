453430734f560e7cea6917718125a08ad9f16456
7lad48 <2335213@gmail.com>
2023-11-27 23:25:06 +0300

    add some styles for preloader

--- src/components/common/Preloader/Preloader.tsx
+++ src/components/common/Preloader/Preloader.tsx
@@ -3,9 +3,9 @@ import preloader from "../../../assets/images/loader.gif";
 import styles from './Preloader.module.css'
 
 export const Preloader = () => {
-    return <div>
-                <div>
-                    <img src={preloader} alt={'preloader'}/>
+    return <div className={styles.preloaderWrapper}>
+                <div className={styles.preloaderImageWrapper}>
+                    <img className={styles.preloaderImg} src={preloader} alt={'preloader'}/>
                 </div>
            </div>
 }
